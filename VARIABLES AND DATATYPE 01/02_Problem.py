8854475aefe7fd38009d978aa1a87de5fe4bd633
Akash Porey <akashporeyofficial2507@gmail.com>
2025-09-15 01:12:58 +0530

    Add New File

---
+++ VARIABLES AND DATATYPE 01/02_Problem.py
@@ -0,0 +1,6 @@
+# Write a python program to find remainder when a number is divided by z.
+
+num1=int(input("Enter the value of num1: "))
+num2=int(input("Enter the value of num2: "))
+
+print("The Remainder is", num1%num2)
