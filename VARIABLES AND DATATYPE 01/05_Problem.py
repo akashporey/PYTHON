291fba0dcadb682cdff3a3e70d4ca3783255a12a
Akash Porey <akashporeyofficial2507@gmail.com>
2025-09-15 01:21:41 +0530

    Add New File

---
+++ VARIABLES AND DATATYPE 01/05_Problem.py
@@ -0,0 +1,7 @@
+# Write a python program to find an average of two numbers entered by the user.
+
+
+a=int(input("Enter the number 1: "))
+b=int(input("Enter the number 2: "))
+
+print("Avarage of this two number is", (a+b)/2)
