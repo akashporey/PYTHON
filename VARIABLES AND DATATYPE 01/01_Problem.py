c81eda00002bee02a5acb16849ca35906b542532
Akash Porey <akashporeyofficial2507@gmail.com>
2025-09-17 23:57:49 +0530

    Delete VARIABLES AND DATATYPE 01 directory

--- VARIABLES AND DATATYPE 01/01_Problem.py
+++
@@ -1,6 +0,0 @@
-# Write a python program to add two numbers.
-
-a= int(input("Enter the value of a: "))
-b=int(input("Enter the value of b: "))
-
-print("The sum is", a+b)
--- VARIABLES AND DATATYPE 01/02_Problem.py
+++
@@ -1,6 +0,0 @@
-# Write a python program to find remainder when a number is divided by z.
-
-num1=int(input("Enter the value of num1: "))
-num2=int(input("Enter the value of num2: "))
-
-print("The Remainder is", num1%num2)
