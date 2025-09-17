0f5638dc92706cba54fb9970c8f2dd81b9f70a51
Akash Porey <akashporeyofficial2507@gmail.com>
2025-09-17 23:57:37 +0530

    Delete STRINGS 02 directory

--- STRINGS 02/01_Problem.py
+++
@@ -1,5 +0,0 @@
-# Write a python program to display a user entered name followed by Good Afternoon using input () function.
-
-Name=input("Enter the name: ")
-
-print(f"Good Afternoon {Name}")
--- STRINGS 02/03_Problem.py
+++
@@ -1,5 +0,0 @@
-# Write a program to detect double space in a string.
-
-kuchbhi=("Hello, I am  Akash Porey")
-
-print(kuchbhi.find("  "))
