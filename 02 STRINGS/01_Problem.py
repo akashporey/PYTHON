b11abb762c9a6085dbb3d6635559f25cb48d2070
EshaMohamed <eshamohamed247@gmail.com>
2025-09-19 17:55:51 +0530

    Update 01_Problem.py

--- 02 STRINGS/01_Problem.py
+++ 02 STRINGS/01_Problem.py
@@ -1,5 +1,6 @@
 # Write a python program to display a user entered name followed by Good Afternoon using input () function.
 
 Name=input("Enter the name: ")
-
+age=int(input("enter the age:"))
 print(f"Good Afternoon {Name}")
+print(f"your age is  {age}")
