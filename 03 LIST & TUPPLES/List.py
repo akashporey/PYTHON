d17b389b04ef2b8c4a5c693134b813367e887cd4
Akash Porey <akashporeyofficial2507@gmail.com>
2025-09-20 22:19:40 +0530

    Add New File

---
+++ 03 LIST & TUPPLES/List.py
@@ -0,0 +1,22 @@
+L1=["Akash", "Porey", 1234, True, "Computer"]
+L1=[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+L1.sort()
+print(L1)
+
+L1.reverse()
+print(L1)
+
+L1.append(11)
+print(L1)
+
+L1.insert(3,11)
+print(L1)
+
+L1.pop(3)
+print(L1)
+
+L1.remove(11)
+print(L1)
+
+print(type(L1)) 
