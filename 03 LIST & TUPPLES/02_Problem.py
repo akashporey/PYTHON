bb5f91468c5bae02d579ad40d9d21ed43681313c
Akash Porey <akashporeyofficial2507@gmail.com>
2025-09-20 22:11:55 +0530

    Add New File

---
+++ 03 LIST & TUPPLES/02_Problem.py
@@ -0,0 +1,29 @@
+# Write a program to accept marks of 6 students and display them in a sorted manner.
+
+Marks = []
+
+N1=int(input("Enter Marks: "))
+Marks.append(N1)
+
+N2=int(input("Enter Marks: "))
+Marks.append(N2)
+
+N3=int(input("Enter Marks: "))
+Marks.append(N3)
+
+N4=int(input("Enter Marks: "))
+Marks.append(N4)
+
+N5=int(input("Enter Marks: "))
+Marks.append(N5)
+
+N6=int(input("Enter Marks: "))
+Marks.append(N6)
+
+N7=int(input("Enter Marks: "))
+Marks.append(N7)
+
+print(Marks)
+
+Marks.sort()
+print(Marks)
