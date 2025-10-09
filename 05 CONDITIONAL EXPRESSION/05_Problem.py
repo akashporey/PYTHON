3a27ea0bb75e2fb9de187eeefe6400ed5603471a
Akash Porey <akashporeyofficial2507@gmail.com>
2025-10-09 23:41:08 +0530

    Add New File

---
+++ 05 CONDITIONAL EXPRESSION/05_Problem.py
@@ -0,0 +1,11 @@
+# Write a program which finds out whether a given name is present in a list or not.
+
+N = ["Akash", "Suman", "Mehphug", "Baiju", "Arslan"]
+
+l = input("Enter your name: ")
+
+if(l in N) :
+    print("Your name is in the list")
+
+else:
+    print("Your name is not in the list")
