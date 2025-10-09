caf73cc3961187aa64b8f2bd5f3babfcb49ebdd1
Akash Porey <akashporeyofficial2507@gmail.com>
2025-10-09 23:31:39 +0530

    Add New File

---
+++ 04  DICTIONARY & SETS/Dic.py
@@ -0,0 +1,14 @@
+a = {
+"Akash": 80,
+"harry": 60,
+"Suman": 99,
+"list": [1, 2, 9]
+}
+print(a.keys())    
+print(a.values()) 
+print(a["list"]) 
+print(a.items())
+a.update({"Akash":90, "Mehphug":95})
+print(a) 
+
+print(a.popitem())
