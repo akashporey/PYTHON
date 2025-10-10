af0610669e411309c02bb372925b8e51f03f2d63
Akash Porey <akashporeyofficial2507@gmail.com>
2025-10-10 23:54:20 +0530

    Add New File

---
+++ 05 CONDITIONAL EXPRESSION/e_validation_regex.py
@@ -0,0 +1,9 @@
+import re
+
+email_condition=r"^[a-z]+[\._]?[a-z 1-9]+[@]\w+[.]\w{2,3}$"
+user_email=input('Enter your email: ')
+
+if re.search(email_condition, user_email):
+    print("Right Email")
+else :
+    print("Wrong Email")
