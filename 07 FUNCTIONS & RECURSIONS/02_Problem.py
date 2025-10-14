4e144750b6c7a783d74c12ca44d9bc692b5b6286
Akash Porey <akashporeyofficial2507@gmail.com>
2025-10-14 23:48:25 +0530

    Add New File

---
+++ 07 FUNCTIONS & RECURSIONS/02_Problem.py
@@ -0,0 +1,8 @@
+# Write a python program using function to convert Celsius to Fahrenheit.
+
+def convert(C):
+    F= (9*C+160)/5
+    return F
+
+C=int(input("Enter temp in Celsius: "))
+print(f"Temp in Fahrenheit: {convert(C)}")
