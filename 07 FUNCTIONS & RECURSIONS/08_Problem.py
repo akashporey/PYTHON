95236eb3d02ccfcdfa8737902c2be815167a3a74
Akash Porey <akashporeyofficial2507@gmail.com>
2025-10-16 23:29:53 +0530

    Add New File

---
+++ 07 FUNCTIONS & RECURSIONS/08_Problem.py
@@ -0,0 +1,12 @@
+# Write a python function to print multiplication table of a given number.
+
+def mul_table(n):
+    if(n==0):
+        return
+    print(f"Multiplication table of {n} is: ")
+    for i in range(1,11):
+        print(n, "X", i, "=", n*i)
+        i+=1
+
+
+mul_table(int(input("Enter a number: ")))
