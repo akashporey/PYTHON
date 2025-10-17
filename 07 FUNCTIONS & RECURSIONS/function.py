ce6b77cc51ed17c4feba64b4f79ae55c7b9ffcb0
Akash Porey <akashporeyofficial2507@gmail.com>
2025-10-18 00:51:13 +0530

    Add New File

---
+++ 07 FUNCTIONS & RECURSIONS/function.py
@@ -0,0 +1,17 @@
+def greet():
+    print("Good Day")
+
+greet()
+print("Thank You")
+greet()
+print("Thank You")
+greet()
+print("Thank You")
+
+print("\n")
+
+def greet(name,ending="Thank You"): #default ending if not mention later
+    print("Good day",name,"\n",ending)
+
+greet("Akash", "Thanks")
+
