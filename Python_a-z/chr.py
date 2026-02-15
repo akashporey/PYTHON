b1c3a7d1421e51692b13ceac233e43ffa0b4af9d
Akash Porey <akashporeyofficial2507@gmail.com>
2026-02-16 00:11:35 +0530

    Add Code

---
+++ Python_a-z/chr.py
@@ -0,0 +1,12 @@
+''' By the help of chr function we can find out the corresponding 
+character of any ASCII value.   '''
+
+x=97
+y=65
+k=32
+l=100
+
+print(chr(x))
+print(chr(y))
+print(chr(k))
+print(chr(l))
