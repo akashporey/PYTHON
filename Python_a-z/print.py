c150c621e182598d518321fd1b2b3dfb5ec26aba
Akash Porey <akashporeyofficial2507@gmail.com>
2026-02-19 23:13:53 +0530

    Add Code

--- Python_a-z/print.py
+++ Python_a-z/print.py
@@ -6,3 +6,14 @@ z= complex()
 print(x)
 print(y)
 print(z)
+
+print("_______________________")
+
+def A():
+    x=10
+    y=30
+    print(x)
+    print(y)
+    return "Hello Friends"
+z= A()
+print(z)
