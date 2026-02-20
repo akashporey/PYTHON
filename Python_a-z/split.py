542c9e7e8df2a9efed856328a62314094e40f3f9
Akash Porey <akashporeyofficial2507@gmail.com>
2026-02-20 23:57:16 +0530

    Add File

---
+++ Python_a-z/split.py
@@ -0,0 +1,11 @@
+## split
+
+x,y,z= input("Enter the values: ").split(" ")
+print(x)
+print(y)
+print(z)
+print("______________")
+
+x, *y= input("Enter the values: ").split("#")
+print(x)
+print(y)
