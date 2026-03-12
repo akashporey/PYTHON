cee2b51949919a2788f7401d6d6ef7bd2e86e4ba
Akash Porey <akashporeyofficial2507@gmail.com>
2026-03-12 23:55:10 +0530

    Modify Code

--- Python_a-z/factorial2.py
+++ Python_a-z/factorial2.py
@@ -1,13 +1,14 @@
-
+## Factorial of each digit of input value
 
 num= int(input("Enter the number: "))
 temp=num
-fact=1
+
 while(temp!=0):
     rem=temp%10
+    fact=1
     temp2=rem
     while(rem!=0):
         fact=fact*rem
         rem=rem-1
-print(f"The factorial of {temp2} is: {fact}")
-temp=temp//10
+    print(f"The factorial of {temp2} is: {fact}")
+    temp=temp//10
