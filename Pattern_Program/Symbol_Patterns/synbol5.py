4490b86fa503351a60b3acb3aef66c8b3056f948
Akash Porey <akashporeyofficial2507@gmail.com>
2026-04-04 02:11:19 +0530

    Modify Code

--- Pattern_Program/Symbol_Patterns/synbol5.py
+++ Pattern_Program/Symbol_Patterns/synbol5.py
@@ -7,8 +7,8 @@
 """
 
 for i in range(1,6):
-    for j in range(1,i+1):
-        print(" ", end="")
-    for k in range(1,6,-1):
-        print("*")
+    for j in range(1,i):
+        print("  ", end="")
+    for k in range(i,10):
+        print("* ",end="")
     print()
