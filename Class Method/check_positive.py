762ea976c74b81363a1a35f6624fb14d1b1cfea0
Akash Porey <akashporeyofficial2507@gmail.com>
2026-04-09 23:55:21 +0530

    Add Code

---
+++ Class Method/check_positive.py
@@ -0,0 +1,11 @@
+class number:
+    def __init__(self,num):
+        self.num=num
+    def is_positve(self):
+        if(num>0):
+            print("It's a positive number: ")
+        else:
+            print("It's not a positive number")
+
+num= int(input("Enter a number: "))
+n1=(number.is_positve)
