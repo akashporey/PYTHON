abf373ad92efe2592044d9f221e23332dd9b4d29
Akash Porey <akashporeyofficial2507@gmail.com>
2026-04-10 00:19:57 +0530

    Modified Code

--- Class Method/check_positive.py
+++ Class Method/check_positive.py
@@ -2,10 +2,13 @@ class number:
     def __init__(self,num):
         self.num=num
     def is_positve(self):
-        if(num>0):
-            print("It's a positive number: ")
+        if(self.num>0):
+            print("It's a positive number")
+        elif(self.num==0):
+            print("It's Neither positive nor negetive")
         else:
-            print("It's not a positive number")
+            print("It's a negetive number")
 
 num= int(input("Enter a number: "))
-n1=(number.is_positve)
+n1=number(num)
+n1.is_positve()
